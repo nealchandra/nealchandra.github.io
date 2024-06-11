c5211c79d21bcbb0cf808afe13bf05ecc4b92fe5
Neal Chandra <chandra.neal@gmail.com>
2024-06-11 14:39:26 -0700

    Omit projects from RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,13 +3,9 @@ import { SITE } from "@consts";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
+  const blog = (await getCollection("blog")).filter((post) => !post.data.draft); 
 
-  const projects = (await getCollection("projects")).filter(
-    (project) => !project.data.draft,
-  );
-
-  const items = [...blog, ...projects].sort(
+  const items = blog.sort(
     (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
   );
 
